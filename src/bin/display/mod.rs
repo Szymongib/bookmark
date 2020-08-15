0ade4df322c669a1454595434a42aa96b5a8628b
Szymongib <szymongib@gmail.com>
2020-08-15 07:54:52 +0200

    Add testcases

--- src/bin/display/mod.rs
+++ src/bin/display/mod.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use bookmark_lib::types::URLRecord;
 
-// // TODO: Add more test cases
 pub(crate) fn display_urls(urls: Vec<URLRecord>) {
     println!("{}", display_str(urls))
 }
@@ -29,7 +28,6 @@ fn display_str(urls: Vec<URLRecord>) -> String {
     return out
 }
 
-// TODO: test
 fn header(id_len: usize, name_len: usize, url_len: usize, group_len: usize, tags_len: usize) -> String {
     let id = pad("Id".to_string(), id_len);
     let name = pad("Name".to_string(), name_len);
@@ -42,7 +40,14 @@ fn header(id_len: usize, name_len: usize, url_len: usize, group_len: usize, tags
 
 fn pad(s: String, len: usize) -> String {
     let mut s = s.clone();
-    for _ in 0..(len - s.len()) {
+
+    let pad_count = if len >= s.len() {
+        len - s.len()
+    } else {
+        0
+    };
+
+    for _ in 0..pad_count {
         s.push(' ');
     }
     return s
@@ -50,7 +55,7 @@ fn pad(s: String, len: usize) -> String {
 
 /// Returns max length of Name, URL, Group, Tags
 fn get_max_lengths(urls: &Vec<URLRecord>) -> (usize, usize, usize, usize) {
-    let mut max_len: [usize; 4] = [0,0,0,0];
+    let mut max_len: [usize; 4] = [4,3,5,0];
 
     for u in urls {
         if u.name.len() > max_len[0] {
@@ -76,33 +81,64 @@ mod test {
     use bookmark_lib::types::URLRecord;
     use crate::display::{display_urls, display_str};
 
-    fn fix_url_records() -> Vec<URLRecord> {
-        vec![
-            URLRecord::new("https://one_long_url.com", "one_name", "one", vec!["tag"]),
-            URLRecord::new("two", "two long name wow such name", "two_long_group", vec![]),
-            URLRecord::new("three", "three", "three", vec![]),
-            URLRecord::new("four.com", "four mid len", "4", vec!["tag"]),
-            URLRecord::new("five", "five", "five", vec!["just_one_but_long_tag_much_wow"]),
-        ]
+    struct TestCase {
+        description: String,
+        records: Vec<URLRecord>,
+        expected_lines: Vec<String>,
     }
 
     #[test]
     fn test_display_str() {
-        let display = display_str(fix_url_records());
 
-        let lines: Vec<&str> = display.split("\n").collect();
-        let expected_lines = vec![
-        "Id                                         Name                          URL                        Group            Tags                          ",
-        "",
-        "2672d7142749ea753a95357d4c0df2d8d8992c6e   one_name                      https://one_long_url.com   one              tag                           ",
-        "74a2b210033dbca577a5a747628ed734a01e897d   two long name wow such name   two                        two_long_group                                 ",
-        "24479e74236b80dc0a0cc67477e8e87d89bfb3cb   three                         three                      three                                          ",
-        "da0477157d1ae3297f9ad9c840815c4f9152d52c   four mid len                  four.com                   4                tag                           ",
-        "ecc5f6712a7a134618fbdf043c7c80f2e30cb870   five                          five                       five             just_one_but_long_tag_much_wow",
+        let test_cases = vec![
+            TestCase{
+                description: "Several URL records".to_string(),
+                records: vec![
+                    URLRecord::new("https://one_long_url.com", "one_name", "one", vec!["tag"]),
+                    URLRecord::new("two", "two long name wow such name", "two_long_group", vec![]),
+                    URLRecord::new("three", "three", "three", vec![]),
+                    URLRecord::new("four.com", "four mid len", "4", vec!["tag"]),
+                    URLRecord::new("five", "five", "five", vec!["just_one_but_long_tag_much_wow"]),
+                ],
+                expected_lines: vec![
+                    "Id                                         Name                          URL                        Group            Tags                          ".to_string(),
+                    "".to_string(),
+                    "2672d7142749ea753a95357d4c0df2d8d8992c6e   one_name                      https://one_long_url.com   one              tag                           ".to_string(),
+                    "74a2b210033dbca577a5a747628ed734a01e897d   two long name wow such name   two                        two_long_group                                 ".to_string(),
+                    "24479e74236b80dc0a0cc67477e8e87d89bfb3cb   three                         three                      three                                          ".to_string(),
+                    "da0477157d1ae3297f9ad9c840815c4f9152d52c   four mid len                  four.com                   4                tag                           ".to_string(),
+                    "ecc5f6712a7a134618fbdf043c7c80f2e30cb870   five                          five                       five             just_one_but_long_tag_much_wow".to_string(),
+                ],
+            },
+            TestCase{
+                description: "Single URL record".to_string(),
+                records: vec![
+                    URLRecord::new("https://httpbin.org", "HTTP Bin", "default", vec!["testing"]),
+                ],
+                expected_lines: vec![
+                    "Id                                         Name       URL                   Group     Tags   ".to_string(),
+                    "".to_string(),
+                    "71af907dc3b7b45a4562cfc1ba68ebe2b81fff88   HTTP Bin   https://httpbin.org   default   testing".to_string(),
+                ],
+            },
+            TestCase{
+                description: "No URL records".to_string(),
+                records: vec![],
+                expected_lines: vec![
+                    "Id   Name   URL   Group   Tags".to_string(),
+                    "".to_string(),
+                ],
+            }
         ];
 
-        for i in 0..lines.len() {
-            assert_eq!(lines[i], expected_lines[i])
+        for test in test_cases {
+            println!("Test: {}", test.description);
+            let display = display_str(test.records);
+
+            let lines: Vec<&str> = display.split("\n").collect();
+            for i in 0..lines.len() {
+                assert_eq!(lines[i], test.expected_lines[i])
+            }
         }
     }
 }
